d76cb217693b9dc87443dad07b743fc8b211fdce
MathiTC <mathiastc@clausenwebworks.com>
2024-05-08 11:31:13 +0200

    Basic SPA page setup made

--- main.py
+++ main.py
@@ -1,16 +1,105 @@
-# This is a sample Python script.
+import tkinter as tk
+from tkinter import ttk
+import pickle
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class HomePage(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+
+        label = tk.Label(self, text="Home Page")
+        label.pack(pady=10, padx=10)
+
+        button1 = tk.Button(self, text="Manage Contexts", command=lambda: controller.show_frame("Contexts"))
+        button1.pack()
+
+        button2 = tk.Button(self, text="Manage Elemtents", command=lambda: controller.show_frame("Elements"))
+        button2.pack()
+
+        button3 = tk.Button(self, text="Rate", command=lambda: controller.show_frame("Rating"))
+        button3.pack()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Contexts(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+
+        label = tk.Label(self, text="Contexts")
+        label.pack(pady=10, padx=10)
+
+        self.back_button = tk.Button(self, text="Back to Home", command=lambda: controller.show_frame("HomePage"))
+        self.back_button.pack()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+class Elements(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+
+        label = tk.Label(self, text="Elements")
+        label.pack(pady=10, padx=10)
+
+        self.back_button = tk.Button(self, text="Back to Home", command=lambda: controller.show_frame("HomePage"))
+        self.back_button.pack()
+
+
+
+class Rating(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+
+        label = tk.Label(self, text="Rating")
+        label.pack(pady=10, padx=10)
+
+        self.back_button = tk.Button(self, text="Back to Home", command=lambda: controller.show_frame("HomePage"))
+        self.back_button.pack()
+
+
+
+
+class Voting(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+
+        label = tk.Label(self, text="Rating")
+        label.pack(pady=10, padx=10)
+
+
+
+
+class SinglePageApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("FodboldTur")
+
+        self.container = tk.Frame(self)
+        self.container.pack(side="top", fill="both", expand=True)
+
+        self.frames = {}
+        self.create_frames()
+
+        self.show_frame("HomePage")
+
+    def create_frames(self):
+        # Define all frames/pages here
+        pages = [HomePage, Contexts, Elements, Rating, Voting]
+
+        for F in pages:
+            frame = F(self.container, self)
+            self.frames[F.__name__] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+    def show_frame(self, page_name):
+        # Show a frame for the given page name
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+
+if __name__ == "__main__":
+    app = SinglePageApp()
+    app.mainloop()
